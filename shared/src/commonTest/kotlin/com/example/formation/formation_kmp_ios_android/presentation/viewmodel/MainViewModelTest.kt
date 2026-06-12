fe676e775d9c2fc3716c53ad87ffe5cf9ae8a803
Vincent Verrier <vincent.verrier@groupe-mma.fr>
2026-06-12 14:46:48 +0200

    uncomment test

--- shared/src/commonTest/kotlin/com/example/formation/formation_kmp_ios_android/presentation/viewmodel/MainViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/example/formation/formation_kmp_ios_android/presentation/viewmodel/MainViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.example.formation.formation_kmp_ios_android.presentation.viewmodel
 
+import com.example.formation.formation_kmp_ios_android.di.apiModule
 import com.example.formation.formation_kmp_ios_android.di.fakeApiModule
 import com.example.formation.formation_kmp_ios_android.domain.WeatherRepository
+import com.example.formation.formation_kmp_ios_android.rest.KtorWeatherApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.koin.core.context.startKoin
@@ -14,7 +16,6 @@ import kotlin.time.Duration.Companion.seconds
 
 
 class MainViewModelTest {
-    /*
     @Test
     fun testAPIWork() = runTest(timeout = 10.seconds) {
 
@@ -30,7 +31,6 @@ class MainViewModelTest {
         }
         assertTrue(list.isNotEmpty())
     }
-     */
 
     @Test
     fun testFakeAPIWork() = runTest(timeout = 10.seconds) {
